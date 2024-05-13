ce86a8e74caaa09743da71304ae148e0f150cb98
Neil Ward <nward@dassian.com>
2024-05-12 17:07:52 -0700

    1st successful oauth!

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CustomUserCreate
+from .views import CustomUserCreate, BungieTokenExchange
 
 app_name = 'users'
 
 urlpatterns = [
     path('create/', CustomUserCreate.as_view(), name="create_user"),
+    path('bungie/', BungieTokenExchange.as_view(), name="bungie_token_exchange"),
+    
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+import requests
+from django.conf import settings
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -16,3 +18,23 @@ class CustomUserCreate(APIView):
                 json = serializer.data
                 return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class BungieTokenExchange(APIView):
+    def post(self, request, *args, **kwargs):
+        code = request.data.get('code')
+        url = "https://www.bungie.net/platform/app/oauth/token/"
+        payload = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'client_id': settings.SOCIAL_AUTH_BUNGIE_KEY,
+            'client_secret': settings.SOCIAL_AUTH_BUNGIE_SECRET,
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        response = requests.post(url, headers=headers, data=payload)
+        if response.status_code == 200:
+            return Response(response.json(), status=status.HTTP_200_OK)
+        else:
+            return Response(response.json(), status=response.status_code)
