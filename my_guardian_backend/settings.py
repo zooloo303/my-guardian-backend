1c8c23be20c6c82e32d9cf4948f9cddc09f8287a
Neil Ward <nward@dassian.com>
2024-05-31 10:36:41 -0700

    more cors stuff

--- my_guardian_backend/settings.py
+++ my_guardian_backend/settings.py
@@ -126,7 +126,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://my-guardian.onrender.com"
+    "https://my-guardian.onrender.com",
+    "https://my-guardian-backend.onrender.com"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
