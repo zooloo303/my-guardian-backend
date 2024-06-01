32967765b0a4d37c9d8a359cd997898a770654c1
Neil Ward <nward@dassian.com>
2024-05-31 19:16:10 -0700

    oops

--- my_guardian_backend/settings.py
+++ my_guardian_backend/settings.py
@@ -130,7 +130,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://my-guardians.com,"
+    "https://my-guardians.com",
     "http://localhost:3000",
     "https://localhost:3000",
     "http://127.0.0.1:3000",
